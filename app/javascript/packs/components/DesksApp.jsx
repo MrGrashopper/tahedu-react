e3db5067189d644add3acb75950eed2662a07fe6
Daniel Mikolai <d.mikolai@gmx.de>
2020-09-11 11:06:12 +0200

    user reservation in DeskApp draft

--- app/javascript/packs/components/DesksApp.jsx
+++ app/javascript/packs/components/DesksApp.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import DeskItems from "./desk_items";
+import UserReservations from "./userReservations";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class DesksApp extends React.Component {
 
@@ -11,12 +15,18 @@ class DesksApp extends React.Component {
             <DeskItems></DeskItems>
         )
     }
+    renderUserReservations = () => {
+        return(
+            <UserReservations></UserReservations>
+        )
+    }
 
     render() {
         return (
-            <div>
-                {this.renderAllDesks()}
-            </div>
+            <Row>
+                <Col xs={11}>{this.renderAllDesks()}</Col>
+                <Col xs={1}>{this.renderUserReservations()}</Col>
+            </Row>
         )
     }
 }
---
+++ app/javascript/packs/components/userReservations.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import setAxiosHeaders from "./AxiosHeaders";
+
+class UserReservations extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            userReservations: [],
+        };
+    }
+    render() {
+        return (
+            <div> TEST </div>
+        )}
+
+}
+
+export default UserReservations
