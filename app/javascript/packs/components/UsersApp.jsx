093d598be6f57e8f1e0aa0f9c107e1dcb54ca354
Daniel Mikolai <d.mikolai@gmx.de>
2020-09-27 12:00:55 +0200

    styling

--- app/javascript/packs/components/UsersApp.jsx
+++ app/javascript/packs/components/UsersApp.jsx
@@ -84,8 +84,9 @@ class UsersApp extends React.Component {
                                     <div  className="col-xl-12 col-md-12 col-sm-12">
                                         <p>{user.supervisor? `Supervisor` : `Mitglied`}</p>
                                         <div>{user.supervisor?
-                                            <a className="red-text"  type="submit"  onClick={() => this.deleteSupervisor(user.id)}>Erweiterte Rechte entfernen</a> :
-                                            <a className="purple-text"  type="submit"  onClick={() => this.setSupervisor(user.id)}>Erweiterte Rechte vergeben </a>}</div>
+                                            <a className="btn btn-danger"  type="submit"  onClick={() => this.deleteSupervisor(user.id)}>Erweiterte Rechte entfernen</a> :
+                                            <a className="btn btn-primary"  type="submit"  onClick={() => this.setSupervisor(user.id)}>Erweiterte Rechte vergeben </a>}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
